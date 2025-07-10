d532fe2eeea68626122a133f1ca97236f8451d21
Aghav Rajesh <aghavrajesh8@gmail.com>
2025-07-10 16:51:48 +0530

    all files

--- routes/teacher.js
+++ routes/teacher.js
@@ -6,9 +6,15 @@ var exe = require('./../connection');
 
 
 
-router.get("/",function (req, res) {
-  res.send("teacher");
+router.get("/",function(req, res) {
+  res.render("teacher/home.ejs");
 
 });
+router.get("/my_classes",function(req,res){
+  res.render("teacher/my_classes.ejs")
+})
 
+router.get("/assignment",function(req,res){
+  res.render("teacher/assignment.ejs")
+})
 module.exports = router;
