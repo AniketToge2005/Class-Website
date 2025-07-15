a9cdac0ef760da74911e885cf997d38e5bd8a05d
AniketToge2005 <anikittoge@gmail.com>
2025-07-15 16:20:49 +0530

    all files
    all files

--- routes/admin.js
+++ routes/admin.js
@@ -13,6 +13,23 @@ router.get("/class5",function (req, res) {
   res.render("admin/class5.ejs");
 });
 
+//navbar_logo
+
+router.get("/navbar_logo",function(req,res){
+  res.render("admin/navbar_logo.ejs");
+});
+
+router.post("/navbar_logo",async function(req,res){
+
+  var d = req.body;
+  var sql = `INSERT INTO navbar_logo (filename, uploaded_at) VALUES ('${d.filename}','${d.uploaded_at}');`
+  var data = await exe(sql);
+  res.send(data);
+  res.redirect("/navbar_logo");
+});
+
+
+// CREATE TABLE logos ( id INT AUTO_INCREMENT PRIMARY KEY, filename VARCHAR(255) , uploaded_at TIMESTAMP );
 
 
 module.exports = router;
