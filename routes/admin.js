167438b40a9f409b904179c03bc6e29df0d3c8ec
Datta padre <dattapadre7249@gmail.com>
2025-07-20 00:56:23 +0530

    assignments
    datta

--- routes/admin.js
+++ routes/admin.js
@@ -42,38 +42,30 @@ router.post("/about_class",async function(req,res){
 
 
 
-
-
-router.get("/my_classes",function(req,res){
-  res.render("admin/my_classes.ejs")
+router.get("/add_assignments",async function(req,res){
+  res.render("admin/Add_assignments.ejs")
 })
 
-router.get("/assignment",async function(req,res){
-  var sql = `SELECT * FROM assignment`
-  var data = await exe(sql);
-  var obj = {"alist":data}
-  res.render("admin/assignment.ejs",obj)
+router.post("/save_assignments",async function(req,res){
+  var d=req.body;
+
+  if(req.files && req.files.pdf){
+    var pdf=new Date().getTime()+".pdf";
+    req.files.pdf.mv("public/uplaod/"+pdf);
+    console.log(pdf)
+  }
+
+var  sql = `INSERT INTO assignments(std_class_id, title, date, subject,pdf) VALUES (?, ?, ?, ?, ?)`;
+var  data = await exe(sql,[d.class,d.title,d.date,d.subject,pdf]);
+  res.redirect("/admin/add_assignments")
+  // res.send(sql)
 })
-router.post("/save_assignment",async function(req, res) {
-  // console.log("Received assignment data:", req.body);
-  var d = req.body;
-  var sql = "INSERT INTO assignment (batch_name,title,subject,due_date,description) VALUES (?,?,?,?,?)";
-  var data = await exe(sql,[d.batch_name,d.title,d.subject,d.due_date,d.description]);
-  // res.send(data);
-  res.redirect("/teach/assignment")
-  // res.send(req.body);
-});
+
 
 router.get("/study_material",function(req,res){
   res.render("admin/study_material.ejs")
 })
 
-router.get("/attendance",function(req,res){
-  res.render("admin/attendance.ejs")
-})
-router.get("/announcement",function(req,res){
-  res.render("admin/announcement.ejs")
-})
 
 
 
